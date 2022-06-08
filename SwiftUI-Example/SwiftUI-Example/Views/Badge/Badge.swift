fc7f94fbae5591e2bc4358e6e5ca84ed5df6ea24
Amritpal Singh <amritpal.singh@honeywell.com>
2022-06-08 14:40:44 +0530

    added drawings and paths

---
+++ SwiftUI-Example/SwiftUI-Example/Views/Badge/Badge.swift
@@ -0,0 +1,35 @@
+//
+//  Badge.swift
+//  SwiftUI-Example
+//
+//  Created by user on 08/06/22.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    
+    var badgeSymbols: some View {
+        ForEach(0..<8) { index in
+            RotatedBadgeSymbol(angle: .degrees(Double(index) / Double(8)) * 360.0)
+        }
+        .opacity(0.5)
+    }
+    
+    var body: some View {
+        ZStack {
+            BadgeBackground()
+            GeometryReader { geometry in
+                badgeSymbols
+                    .scaleEffect(1.0 / 4.0, anchor: .top)
+                    .position(x: geometry.size.width / 2.0, y: (3.0 / 4.0) * geometry.size.height)
+            }
+        }
+    }
+}
+
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
