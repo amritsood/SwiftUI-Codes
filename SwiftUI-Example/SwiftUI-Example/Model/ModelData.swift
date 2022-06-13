c93a2ae0b6536aa0e667e280a5580899b9e5c5f9
Amritpal Singh <amritpal.singh@honeywell.com>
2022-06-13 10:17:58 +0530

    added category views

--- SwiftUI-Example/SwiftUI-Example/Model/ModelData.swift
+++ SwiftUI-Example/SwiftUI-Example/Model/ModelData.swift
@@ -8,8 +8,20 @@
 import Foundation
 
 final class ModelData: ObservableObject {
+    
     @Published var landmarks: [Landmark] = load("landmarkData")
     var hikes: [Hike] = load("hikeData")
+    
+    var features: [Landmark] {
+        landmarks.filter { $0.isFeatured }
+    }
+    
+    var categories: [String: [Landmark]] {
+        Dictionary(
+            grouping: landmarks,
+            by: { $0.category.rawValue }
+        )
+    }
 }
 
 func load<T: Decodable>(_ filename: String) -> T {
---
+++ SwiftUI-Example/SwiftUI-Example/Views/CategoryItem.swift
@@ -0,0 +1,33 @@
+//
+//  CategoryItem.swift
+//  SwiftUI-Example
+//
+//  Created by user on 10/06/22.
+//
+
+import SwiftUI
+
+struct CategoryItem: View {
+    
+    var landmark: Landmark
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            landmark
+                .image
+                .resizable()
+                .frame(width: 150, height: 150)
+                .cornerRadius(5)
+            Text(landmark.name)
+                .font(.caption)
+        }
+        .padding(.leading, 15)
+    }
+}
+
+struct CategoryItem_Previews: PreviewProvider {
+    static var previews: some View {
+        let landmark = ModelData().landmarks.first!
+        CategoryItem(landmark: landmark)
+    }
+}
